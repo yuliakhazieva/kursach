69097543622c524e6f5b9ce14f195c4de7efcc0d
Nanglo <oppasaranhae@gmail.com>
2018-04-26 20:19:33 +0300

    чето выводится но ловлю лимт

--- app/views.py
+++ app/views.py
@@ -36,12 +36,40 @@ def login():
     form = LoginForm()
     if request.method == 'POST':
         aaa = form.userid.data
-        subscriptions = api.users.getSubscriptions(user_id=int(aaa), extended = 1, count = 10, version = 5.0)['items'];
-        df = pd.DataFrame({'userid': []})
-        for i in subscriptions:
-            if api.groups.getMembers(i['id'], "id_asc", 0, 1)['count'] < 1000000:
+        subscriptions = api.users.getSubscriptions(user_id=int(aaa), extended = 1, count = 10, version = 5.0)
+        df = pd.DataFrame({'userid': [0]})
+        #идём по группам нашего пользователя
+        for group in subscriptions:
+            memberCount = api.groups.getMembers(group_id=group['gid'], sort='id_asc', offset=0,count=0, version = 5.0)['count']
+            if memberCount  < 1000000:
+                #для каждого куска по 1000 подписчиков этого паблика
+                print(group['name'] + '\n\n\n\n')
 
-                sys.stdout.write(i['name'])
+                for i in range (1, memberCount/1000):
+                    members = api.groups.getMembers(group_id=group['gid'], sort="id_asc", offest=i, version = 5.0)
+                    #для каждого пользователя
+                    for member in members['users']:
+                        print('memderid\n\n\n\n')
+                        print(member)
+                        memberSubs = api.users.getSubscriptions(user_id=member, extended = 1, count = 10, version = 5.0);
+                        df.append([0], ignore_index=True)
+                        #идём по всем его подпискам и добавляем в табличку
+                        howHighUp = 10;
+                        print('membersubs\n\n\n\n\n\n')
+                        for memberSub in memberSubs:
+                            print(memberSub['gid'])
+                            howHighUp -= 1;
+                            if memberSub['gid'] in df:
+                                df.at[df.shape[0] - 1, memberSub['gid']] = howHighUp;
+                            else:
+                                df[memberSub['gid']] = 0
+                                df.at[df.shape[0] - 1, memberSub['gid']] = howHighUp;
+
+                            for column in df:
+                                if not df[column].iloc(df.shape[0] - 1):
+                                    df.at[df.shape[0] - 1, column] = 0
+
+
 
     if form.validate_on_submit():
         return redirect('/index')
