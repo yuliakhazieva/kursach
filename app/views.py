f3e9d60f1fa51a50cc36015321d3308b84d567d6
Nanglo <oppasaranhae@gmail.com>
2018-04-28 13:50:34 +0300

    все работает но долго пиздец

--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@
 # vim:fileencoding=utf-8
 import sys
 import pandas as pd
+import time
 reload(sys)
 sys.setdefaultencoding('utf-8')
 from flask import render_template, redirect, request
@@ -33,46 +34,48 @@ def index():
 
 @app_this.route('/login', methods = ['GET', 'POST'])
 def login():
+    memebersearchedcount = 0
     form = LoginForm()
     if request.method == 'POST':
         aaa = form.userid.data
-        subscriptions = api.users.getSubscriptions(user_id=int(aaa), extended = 1, count = 10, version = 5.0)
+        subscriptions = api.users.getSubscriptions(user_id=int(aaa), extended = 1, count = 20, version = 5.0, timeout=10)
         df = pd.DataFrame({'userid': [0]})
         #идём по группам нашего пользователя
         for group in subscriptions:
-            memberCount = api.groups.getMembers(group_id=group['gid'], sort='id_asc', offset=0,count=0, version = 5.0)['count']
+            memberCount = api.groups.getMembers(group_id=group['gid'], sort='id_asc', offset=0,count=0, version = 5.0, timeout=10)['count']
             if memberCount  < 1000000:
                 #для каждого куска по 1000 подписчиков этого паблика
-                print(group['name'] + '\n\n\n\n')
-
                 for i in range (1, memberCount/1000):
-                    members = api.groups.getMembers(group_id=group['gid'], sort="id_asc", offest=i, version = 5.0)
+                    members = api.groups.getMembers(group_id=group['gid'], sort="id_asc", offest=i, version = 5.0, timeout=10)
                     #для каждого пользователя
                     for member in members['users']:
-                        print('memderid\n\n\n\n')
-                        print(member)
-                        memberSubs = api.users.getSubscriptions(user_id=member, extended = 1, count = 10, version = 5.0);
-                        df.append([0], ignore_index=True)
-                        #идём по всем его подпискам и добавляем в табличку
-                        howHighUp = 10;
-                        print('membersubs\n\n\n\n\n\n')
-                        for memberSub in memberSubs:
-                            print(memberSub['gid'])
-                            howHighUp -= 1;
-                            if memberSub['gid'] in df:
-                                df.at[df.shape[0] - 1, memberSub['gid']] = howHighUp;
-                            else:
-                                df[memberSub['gid']] = 0
-                                df.at[df.shape[0] - 1, memberSub['gid']] = howHighUp;
-
-                            for column in df:
-                                if not df[column].iloc(df.shape[0] - 1):
-                                    df.at[df.shape[0] - 1, column] = 0
-
-
+                        memebersearchedcount+=1
+                        print(memebersearchedcount)
+                        memberSubs = api.users.getSubscriptions(user_id=member, extended = 1, count = 20, version = 5.0, timeout=10);
+                        if checkForTripleMatch(memberSubs, subscriptions, member):
+                            df.loc[df.shape[0]] = [0 for n in range(df.shape[1])]
+                            #идём по всем его подпискам и добавляем в табличку
+                            howHighUp = 21;
+                            for memberSub in memberSubs:
+                                if'gid' in memberSub:
+                                    howHighUp -= 1;
+                                    if memberSub['gid'] in df:
+                                        df.at[df.shape[0] - 1, memberSub['gid']] = howHighUp;
+                                    else:
+                                        df[memberSub['gid']] = 0
+                                        df.at[df.shape[0] - 1, memberSub['gid']] = howHighUp;
 
     if form.validate_on_submit():
         return redirect('/index')
     return render_template('login.html', 
         title = 'Sign In',
         form = form)
+
+def checkForTripleMatch(membersubs, subscriptions, member):
+    count = 0
+    for sub in membersubs:
+        if sub in subscriptions:
+            count+=1;
+            if count == 3:
+                return True
+    return False
